4e4c05c8161e63e2d42af8fa0d566fb7a1249aa7
Shivam Mathur <shivam.mathurg10@gmail.com>
2025-08-18 08:18:57 +0530

    feat: add prisma generate to postinstall script for production builds

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
   trailingSlash: false,
 };
 
